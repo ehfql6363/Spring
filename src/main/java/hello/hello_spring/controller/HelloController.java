f3ce86d9a33ee1376876d8ec173a5d8fcd1e462c
김동열 <ehfql6363@naver.com>
2022-05-20 03:28:37 +0900

    Inflearn Spring Basic - 20220520

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,18 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "spring!!");
+        //data라는 이름에 hello!!라는 값이 들어감
+        //data = hello!!라는 식?
+        return "hello";
+        // resources\templates\파일 이름(=ViewName).html 을 리턴하여 랜더링하게 함.
+    }
+}

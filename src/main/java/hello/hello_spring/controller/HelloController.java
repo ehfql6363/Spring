2c42c76e192590cbe97b238be7a174344656897d
dong yeol <ehfql6363@naver.com>
2022-05-22 01:12:26 +0900

    Inflearn Spring Basic - 20220522

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -21,5 +22,31 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http body부분에 직접 접근하(넣)기 위해 사용
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name; //문자열 그대로 출력(html태그가 없음)
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello(); //Hello객체 생성
+        hello.setName(name);
+        return hello; //객체를 반환 = JSON형식으로 출력
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
